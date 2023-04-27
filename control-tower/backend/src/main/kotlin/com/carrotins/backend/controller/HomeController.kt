1bf8919c90ec173d89269994f6741b572a1d2daa
alvin <alvin@carrotins.com>
2023-04-27 17:59:14 +0900

    first commit

---
+++ control-tower/backend/src/main/kotlin/com/carrotins/backend/controller/HomeController.kt
@@ -0,0 +1,10 @@
+package com.carrotins.backend.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * Created by alvin on 2023/04/27.
+ */
+@RestController
+class HomeController {
+}
