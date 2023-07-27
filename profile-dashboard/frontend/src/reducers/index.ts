3fbc10492e2887d43e2ec90eecabc4726fc9a9d3
alvin <alvin@carrotins.com>
2023-07-27 16:46:16 +0900

    테스트 중

--- profile-dashboard/frontend/src/reducers/index.ts
+++ profile-dashboard/frontend/src/reducers/index.ts
@@ -14,15 +14,15 @@ const mapStyles = {
     voyager: {
         id: 'voyager',
         label: 'Voyager',
-        url: 'https://api.maptiler.com/maps/voyager/style.json?key=ySQ0fIYn7eSl3ppOeEJd'
-        // url: 'http://localhost:8081/kkk.json',
+        // url: 'https://api.maptiler.com/maps/voyager/style.json?key=ySQ0fIYn7eSl3ppOeEJd'
+        url: 'http://localhost:8081/kkk.json',
         // icon: 'https://api.maptiler.com/maps/voyager/256/0/0/0.png?key=ySQ0fIYn7eSl3ppOeEJd'
     },
     terrain: {
         id: 'terrain',
         label: 'Outdoor',
         // url: 'https://api.maptiler.com/maps/outdoor/style.json?key=ySQ0fIYn7eSl3ppOeEJd'
-        url: 'http://localhost:8081/test-style.json?key=ySQ0fIYn7eSl3ppOeEJd',
+        url: 'http://localhost:8081/test-style.json',
         // icon: 'https://openmaptiles.org/img/styles/terrain.jpg',
     }
 };
