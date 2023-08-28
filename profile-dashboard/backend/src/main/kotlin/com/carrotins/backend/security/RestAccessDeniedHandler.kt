8d8d0aa7fbed60ee0d2239d0c32bc8d0368afa99
alvin <alvin@carrotins.com>
2023-08-28 10:44:34 +0900

    로그인 작업중

--- profile-dashboard/backend/src/main/kotlin/com/carrotins/backend/security/RestAccessDeniedHandler.kt
+++ profile-dashboard/backend/src/main/kotlin/com/carrotins/backend/security/RestAccessDeniedHandler.kt
@@ -1,5 +1,6 @@
 package com.carrotins.backend.security
 
+import com.carrotins.backend.utils.logger
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.web.access.AccessDeniedHandler
 import org.springframework.stereotype.Component
@@ -10,12 +11,15 @@ import javax.servlet.http.HttpServletResponse
  * Created by alvin on 2023/08/22.
  */
 @Component
-class RestAccessDeniedHandler: AccessDeniedHandler {
+class RestAccessDeniedHandler : AccessDeniedHandler {
+    private val log = logger<RestAccessDeniedHandler>()
     override fun handle(
         request: HttpServletRequest?,
         response: HttpServletResponse?,
         accessDeniedException: AccessDeniedException?
     ) {
-        response!!.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException!!.message)
+//        response!!.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException!!.message)
+
+        log.error("Response:" + HttpServletResponse.SC_FORBIDDEN + ":" + accessDeniedException!!.message)
     }
 }
--- profile-dashboard/backend/src/main/kotlin/com/carrotins/backend/security/RestAuthenticationEntryPoint.kt
+++ profile-dashboard/backend/src/main/kotlin/com/carrotins/backend/security/RestAuthenticationEntryPoint.kt
@@ -1,5 +1,6 @@
 package com.carrotins.backend.security
 
+import com.carrotins.backend.utils.logger
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
@@ -10,12 +11,15 @@ import javax.servlet.http.HttpServletResponse
  * Created by alvin on 2023/08/22.
  */
 @Component
-class RestAuthenticationEntryPoint: AuthenticationEntryPoint {
+class RestAuthenticationEntryPoint : AuthenticationEntryPoint {
+
+    private val log = logger<RestAuthenticationEntryPoint>()
     override fun commence(
         request: HttpServletRequest?,
         response: HttpServletResponse?,
         authException: AuthenticationException?
     ) {
-        response!!.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException!!.message)
+//        response!!.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException!!.message)
+        log.error("Response:" + HttpServletResponse.SC_UNAUTHORIZED + ":" + authException!!.message)
     }
 }
