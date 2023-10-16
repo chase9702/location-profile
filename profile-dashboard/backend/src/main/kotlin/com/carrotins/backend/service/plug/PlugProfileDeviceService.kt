763556835586403dfb9da0ae893b27d39dd428d0
sewhan97@naver.com <sewhan97@naver.com>
2023-10-16 13:54:52 +0900

    cache clear add

--- profile-dashboard/backend/src/main/kotlin/com/carrotins/backend/service/plug/PlugProfileDeviceService.kt
+++ profile-dashboard/backend/src/main/kotlin/com/carrotins/backend/service/plug/PlugProfileDeviceService.kt
@@ -3,6 +3,7 @@ package com.carrotins.backend.service.plug
 import com.carrotins.backend.repository.plug.DeviceTop100Data
 import com.carrotins.backend.repository.plug.DeviceTripData
 import com.carrotins.backend.repository.plug.PlugDeviceRepository
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 /**
@@ -12,11 +13,11 @@ import org.springframework.stereotype.Service
 class PlugProfileDeviceService(
     private val plugDeviceRepository: PlugDeviceRepository
 ) {
+    @Cacheable("cacheDeviceTop")
     fun getDeviceTopData(deviceGb: String, date: String): List<DeviceTop100Data> {
-
         return plugDeviceRepository.getDeviceTopData(deviceGb, date)
     }
-
+    @Cacheable("cacheTripDataFromTopDevice")
     fun getTripDataFromTopDevice(deviceId: String, deviceGb: String, date: String): List<DeviceTripData> {
         return plugDeviceRepository.getTripDataFromTopDevice(deviceId, deviceGb, date)
     }
