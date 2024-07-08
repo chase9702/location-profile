0e0891e4e9e199c90e8ee3589c73420b02760ab3
alvin <alvin@carrotins.com>
2024-07-08 11:51:52 +0900

    eslint 추가

--- profile-dashboard/frontend/babel.config.js
+++ profile-dashboard/frontend/babel.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    presets: [
-        '@babel/preset-env',
-        '@babel/preset-typescript',
-        '@babel/preset-react'
-    ],
-    plugins: [
-        '@babel/proposal-class-properties', // handle class property
-        '@babel/proposal-object-rest-spread', // handle ... variable spread
-        '@babel/transform-runtime',
-        "@babel/plugin-syntax-dynamic-import",
-        ['import', {'libraryName': 'antd', 'libraryDirectory': 'es', 'style': true}]
-    ]
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/proposal-class-properties', // handle class property
+    '@babel/proposal-object-rest-spread', // handle ... variable spread
+    '@babel/transform-runtime',
+    '@babel/plugin-syntax-dynamic-import',
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
+  ],
 };
--- profile-dashboard/frontend/webpack.config.js
+++ profile-dashboard/frontend/webpack.config.js
@@ -1,233 +1,254 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const Dotenv = require('dotenv-webpack');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const crypto = require('crypto');
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const TerserPlugin = require('terser-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const CompressionPlugin = require('compression-webpack-plugin');
 
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const theme = require('./theme');
-const {ProvidePlugin} = require("webpack");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { ProvidePlugin } = require('webpack');
 
 module.exports = (env, options) => {
-    const outputPath = path.resolve(__dirname, 'build');
-    const mode = !options.mode ? 'development' : options.mode;
-    const outputFilename = mode === 'development' ? 'js/[name].js' : 'js/[name].[contenthash].js';
+  const outputPath = path.resolve(__dirname, 'build');
+  const mode = !options.mode ? 'development' : options.mode;
+  const outputFilename =
+    mode === 'development' ? 'js/[name].js' : 'js/[name].[contenthash].js';
 
-    const isProduction = options.mode === 'production';
+  const isProduction = options.mode === 'production';
 
-    const config = {
-        mode: mode,
-        entry: {
-            app: ['./src/index.tsx']
-        },
-        output: {
-            path: outputPath,
-            publicPath: '/',
-            filename: outputFilename,
-        },
-        optimization: {
-            minimize: isProduction,
-            minimizer: [
-                new CssMinimizerPlugin(),
-                isProduction
-                    ? new TerserPlugin({
-                        terserOptions: {
-                            compress: {
-                                drop_console: true,
-                            },
-                        },
-                    })
-                    : null,
-            ].filter(Boolean),
-            splitChunks: {
-                cacheGroups: {
-                    default: false,
-                    vendors: false,
-                    defaultVendors: false,
-                    core: {
-                        test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
-                        name: 'core',
-                        chunks: 'all',
-                        priority: 60
-                    },
-                    antd: {
-                        test: /[\\/]node_modules[\\/]antd[\\/]/,
-                        name: 'antd',
-                        chunks: 'all',
-                        priority: 50
-                    },
-                    'ant-design': {
-                        test: /[\\/]node_modules[\\/]@ant-design[\\/]/,
-                        name: 'ant-design',
-                        chunks: 'all',
-                        enforce: true,
-                        priority: 40
-                    },
-                    kepler: {
-                        test: /[\\/]node_modules[\\/]kepler.gl[\\/]/,
-                        name: 'kepler',
-                        chunks: 'all',
-                        enforce: true,
-                        priority: 35
-                    },
-                    antv: {
-                        test: /[\\/]node_modules[\\/]@antv[\\/]/,
-                        name: 'antv',
-                        chunks: 'all',
-                        enforce: true,
-                        priority: 30
-                    },
-                    deck: {
-                        test: /[\\/]node_modules[\\/]@deck.gl[\\/]/,
-                        name: 'deck',
-                        chunks: 'all',
-                        enforce: true,
-                        priority: 33
-                    },
-                    loaders: {
-                        test: /[\\/]node_modules[\\/]@loaders.gl[\\/]/,
-                        name: 'loaders',
-                        chunks: 'all',
-                        enforce: true,
-                        priority: 31
-                    },
-                    lib: {
-                        test(module) {
-                            return (module.size() > 80000 && /node_modules[/\\]/.test(module.identifier()));
-                        },
-                        name(module) {
-                            const hash = crypto.createHash('sha1');
-                            hash.update(module.libIdent({context: __dirname}));
-                            return `common.${hash.digest('hex').substring(0, 8)}`;
-                        },
-                        chunks: 'all',
-                        enforce: true,
-                        priority: 20
-                    },
-                    commons: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: 'commons',
-                        chunks: 'all',
-                        enforce: true,
-                        priority: 10
-                    }
-                }
-            }
-        },
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js', '.jsx'],
-            alias: {
-                '@src': path.resolve(__dirname, 'src')
-            },
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(ts|js)x?$/,
-                    exclude: /node_modules/,
-                    use: 'babel-loader',
+  const config = {
+    mode: mode,
+    entry: {
+      app: ['./src/index.tsx'],
+    },
+    output: {
+      path: outputPath,
+      publicPath: '/',
+      filename: outputFilename,
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new CssMinimizerPlugin(),
+        isProduction
+          ? new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true,
                 },
-                {
-                    test: /\.css$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: !isProduction,
-                            },
-                        },
-                    ],
-                },
-                {
-                    test: /\.less$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: !isProduction,
-                            },
-                        },
-                        {
-                            loader: 'less-loader',
-                            options: {
-                                sourceMap: !isProduction,
-                                lessOptions: {
-                                    modifyVars: theme,
-                                    javascriptEnabled: true,
-                                },
-                            },
-                        },
-                    ],
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|svg)$/,
-                    type: 'asset/resource',
-                    generator: {
-                        filename: 'fonts/[name][ext]',
-                    },
-                },
-            ],
-        },
-        plugins: [
-            new Dotenv(),
-            new HtmlWebpackPlugin({
-                template: './public/index.html',
-                filename: path.resolve(outputPath, 'index.html')
-            }),
-            new ProvidePlugin({
-                process: 'process/browser',
-            }),
-            new MiniCssExtractPlugin({
-                filename: isProduction ? 'css/[name].[contenthash].css' : 'css/[name].css',
-            }),
-            isProduction &&
-            new CompressionPlugin({
-                algorithm: 'gzip',
-                test: /\.(js|css|html|svg)$/,
-                threshold: 10240,
-                minRatio: 0.8,
-            }),
-        ].filter(Boolean),
-    };
-
-    if (config.mode === 'development') {
-        config.plugins = [
-            ...config.plugins,
-            new BundleAnalyzerPlugin({
-                openAnalyzer: false
+              },
             })
-        ];
-
-        config.devServer = {
-            static: {
-                directory: outputPath // 정적 파일 경로
+          : null,
+      ].filter(Boolean),
+      splitChunks: {
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          defaultVendors: false,
+          core: {
+            test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
+            name: 'core',
+            chunks: 'all',
+            priority: 60,
+          },
+          antd: {
+            test: /[\\/]node_modules[\\/]antd[\\/]/,
+            name: 'antd',
+            chunks: 'all',
+            priority: 50,
+          },
+          'ant-design': {
+            test: /[\\/]node_modules[\\/]@ant-design[\\/]/,
+            name: 'ant-design',
+            chunks: 'all',
+            enforce: true,
+            priority: 40,
+          },
+          kepler: {
+            test: /[\\/]node_modules[\\/]kepler.gl[\\/]/,
+            name: 'kepler',
+            chunks: 'all',
+            enforce: true,
+            priority: 35,
+          },
+          antv: {
+            test: /[\\/]node_modules[\\/]@antv[\\/]/,
+            name: 'antv',
+            chunks: 'all',
+            enforce: true,
+            priority: 30,
+          },
+          deck: {
+            test: /[\\/]node_modules[\\/]@deck.gl[\\/]/,
+            name: 'deck',
+            chunks: 'all',
+            enforce: true,
+            priority: 33,
+          },
+          loaders: {
+            test: /[\\/]node_modules[\\/]@loaders.gl[\\/]/,
+            name: 'loaders',
+            chunks: 'all',
+            enforce: true,
+            priority: 31,
+          },
+          lib: {
+            test(module) {
+              return (
+                module.size() > 80000 &&
+                /node_modules[/\\]/.test(module.identifier())
+              );
             },
-            host: 'localhost',
-            port: 3000,
-            // open: true,
-            historyApiFallback: true,
-            proxy: [{
-                context: ['/api', '/login', '/logout'],
-                // context: ['/api'],
-                target: 'http://localhost:8080',
-                secure: false
-            }],
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-                'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-                // https: true
-            }
-        };
-        config.devtool = 'inline-source-map';
-    }
+            name(module) {
+              const hash = crypto.createHash('sha1');
+              hash.update(module.libIdent({ context: __dirname }));
+              return `common.${hash.digest('hex').substring(0, 8)}`;
+            },
+            chunks: 'all',
+            enforce: true,
+            priority: 20,
+          },
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'commons',
+            chunks: 'all',
+            enforce: true,
+            priority: 10,
+          },
+        },
+      },
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      alias: {
+        '@src': path.resolve(__dirname, 'src'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !isProduction,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.less$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !isProduction,
+              },
+            },
+            {
+              loader: 'less-loader',
+              options: {
+                sourceMap: !isProduction,
+                lessOptions: {
+                  modifyVars: theme,
+                  javascriptEnabled: true,
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|svg)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][ext]',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new Dotenv(),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: path.resolve(outputPath, 'index.html'),
+      }),
+      new ProvidePlugin({
+        process: 'process/browser',
+      }),
+      new MiniCssExtractPlugin({
+        filename: isProduction
+          ? 'css/[name].[contenthash].css'
+          : 'css/[name].css',
+      }),
+      isProduction &&
+        new CompressionPlugin({
+          algorithm: 'gzip',
+          test: /\.(js|css|html|svg)$/,
+          threshold: 10240,
+          minRatio: 0.8,
+        }),
+    ].filter(Boolean),
+  };
 
-    return config;
+  if (config.mode === 'development') {
+    config.plugins = [
+      ...config.plugins,
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }),
+    ];
+
+    config.devServer = {
+      static: {
+        directory: outputPath, // 정적 파일 경로
+      },
+      host: 'localhost',
+      port: 3000,
+      // open: true,
+      historyApiFallback: true,
+      proxy: [
+        {
+          context: ['/api', '/login', '/logout'],
+          // context: ['/api'],
+          target: 'http://localhost:8080',
+          secure: false,
+        },
+      ],
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods':
+          'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers':
+          'X-Requested-With, content-type, Authorization',
+        // https: true
+      },
+    };
+    config.devtool = 'inline-source-map';
+  }
+
+  return config;
 };
